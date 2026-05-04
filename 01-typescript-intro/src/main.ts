054fcf5013ac091c00f32894dd96a130d299b10a
Andres Luna <miosesfreddy123@gmail.com>
2026-05-04 14:16:56 +0200

    feature/imports

---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,8 @@
+import './style.css';
+
+//import './topics/01-basic-types'
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+app.innerHTML='Hola Mundo '
+console.log('Hola mundo!!!!!!');
+
---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,14 @@
+const name: string = 'Stider';
+let hpPoints : number | 'FULL' =95;
+const isAlive : boolean = true;
+
+
+
+
+hpPoints = 40 , 'FULL'
+
+console.log({
+    name,hpPoints,isAlive
+})
+
+export { };
