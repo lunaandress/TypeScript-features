f4c9fc8527fbaff2f3017f0b0e970bc85c9e0ac4
Andres Luna <miosesfreddy123@gmail.com>
2026-05-04 16:52:46 +0200

    feature/funtions

---
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -0,0 +1,25 @@
+
+//FUNCIONES
+
+function addNUmbers(a : number  , b :number){
+    return a+b;
+}
+
+function multiply(firstNumber: number , secondNmber?: number , base: number=2){
+return firstNumber * base;
+}
+
+const addNumberArrow = (a:number , b:number):string=>{
+return `${a + b}`;
+}
+
+const result=addNUmbers(1,2);
+const result2 : string=addNumberArrow(1,5)
+const multiplyResult: number =multiply(5)
+
+
+
+
+console.log(result,result2 , multiplyResult)
+
+export{}
