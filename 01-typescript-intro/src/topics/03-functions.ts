fe4fb292b8aa66101e5307d9947bf7411ffe3b86
Andres Luna <miosesfreddy123@gmail.com>
2026-05-04 17:15:47 +0200

    feature/functions with  arguments

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -13,13 +13,36 @@ const addNumberArrow = (a:number , b:number):string=>{
 return `${a + b}`;
 }
 
-const result=addNUmbers(1,2);
-const result2 : string=addNumberArrow(1,5)
-const multiplyResult: number =multiply(5)
+//const result=addNUmbers(1,2);
+//const result2 : string=addNumberArrow(1,5)
+//const multiplyResult: number =multiply(5)
+
+//console.log(result,result2 , multiplyResult)
+
+//CREAMOS UNA INTERFAZ DEL PERSONAJE  TIPAR
+
+interface Character{
+    name:string ;
+    hp: number;
+    showHp:()=>void // aqui de esta forma definimos una funcion dentro de una inte
+}
+
+const healCharacter=(character  : Character, amount:number )=>{
+    character.hp += amount
+}
+
+const strider : Character={
+    name:'Strider',
+    hp:50,
+    showHp(){
+        console.log(`Puntos de vida ${this.hp}`)
+    }
+}
 
 
 
-
-console.log(result,result2 , multiplyResult)
+healCharacter(strider,10)
+healCharacter(strider,50)
+strider.showHp();
 
 export{}
