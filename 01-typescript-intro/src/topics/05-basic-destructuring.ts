be3935479f45fc664dcc5f8a82a56fdddb460c3a
Andres Luna <miosesfreddy123@gmail.com>
2026-05-05 11:28:16 +0200

    feature/deconstruction-using-arrays

--- 01-typescript-intro/src/topics/05-basic-destructuring.ts
+++ 01-typescript-intro/src/topics/05-basic-destructuring.ts
@@ -22,16 +22,21 @@ const audioPlayer: AudioPlayer={
         year:2015,
     }
 }
-
-
-
 //La destructuracion es y sirve para tomar de mi const varias piezas que me sirven
 const {song:anotherSong ,songDuration:duration,details}=audioPlayer
 const{author}=details;
-console.log('Song',anotherSong);
-console.log('Duration',duration);
-console.log('author',author);
+//console.log('Song',anotherSong);
+//console.log('Duration',duration);
+//console.log('author',author);
 
+
+//destructurizaccion de los arreglos V1
+const dbz : string[]=['Goku','Vegeta','Trunks'];
+console.log('Personaje 3 :',dbz[3]|| 'No hay personaje ')
+
+//destructurizaccion de los arreglos V2
+const [, , trunks='Not fond'] : string[]=['Goku','Vegeta'];
+console.error('Personaje 3 :', trunks)
 
 
 
