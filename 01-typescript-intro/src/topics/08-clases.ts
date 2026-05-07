a2c57847aae185a24897c30a293ff2600f178b55
Andres Luna <miosesfreddy123@gmail.com>
2026-05-07 14:45:46 +0200

    refactor/prioritize-composition over-inheritance

--- 01-typescript-intro/src/topics/08-clases.ts
+++ 01-typescript-intro/src/topics/08-clases.ts
@@ -1,3 +1,5 @@
+
+
 export class Person{
 
     public name : string;
@@ -14,7 +16,10 @@ export class Person{
     ){}*/
 }
 
-export class Hero extends Person {
+
+/*export class Hero extends Person {
+
+    
 
     constructor(
         public alterEgo: string,
@@ -25,8 +30,30 @@ export class Hero extends Person {
         super(realName,'New York');
     }
 
+}*/
+
+
+//CLASE SIN  EXTENDS   OTRA FROMA DE HACER HERENCIA
+export class Hero {
+
+   // public  person : Person;
+    
+
+    constructor(
+        public alterEgo: string,
+        public age : number,
+        public realName: string,
+        public person : Person,
+
+    
+    ) {
+       // this.person= new Person(realName);
+    }
+
 }
 
 
-const iroman = new Hero('Ironman',45,'Tony');
+
+const tony = new Person('Tony Stark','New York');
+const iroman = new Hero('Ironman',45,'Tony',tony);
 console.log(iroman)
